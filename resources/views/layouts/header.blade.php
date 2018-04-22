b14c3241a136eeea614b46719d932ede83e6d6a4
fabioued <fabioued@yahoo.fr>
2018-04-22 12:00:39 +0800

    links corrected

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,19 +12,19 @@
 <meta property="og:url"            content="https://kooxer.yanfoma.tech">
 <meta property="og:type"           content="website">
 <link rel="stylesheet"             href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-<link rel="apple-touch-icon"       sizes="57x57"         href="href="{{asset('img/favicon/apple-icon-57x57.png')}}">
-<link rel="apple-touch-icon"       sizes="60x60"         href="href="{{asset('img/favicon/apple-icon-60x60.png')}}">
-<link rel="apple-touch-icon"       sizes="72x72"         href="href="{{asset('img/favicon/apple-icon-72x72.png')}}">
-<link rel="apple-touch-icon"       sizes="76x76"         href="href="{{asset('img/favicon/apple-icon-76x76.png')}}">
-<link rel="apple-touch-icon"       sizes="114x114"       href="href="{{asset('img/favicon/apple-icon-114x114.png')}}">
-<link rel="apple-touch-icon"       sizes="120x120"       href="href="{{asset('img/favicon/apple-icon-120x120.png')}}">
-<link rel="apple-touch-icon"       sizes="144x144"       href="href="{{asset('img/favicon/apple-icon-144x144.png')}}">
-<link rel="apple-touch-icon"       sizes="152x152"       href="href="{{asset('img/favicon/apple-icon-152x152.png')}}">
-<link rel="apple-touch-icon"       sizes="180x180"       href="href="{{asset('img/favicon/apple-icon-180x180.png')}}">
-<link rel="icon" type="image/png"  sizes="192x192"       href="href="{{asset('img/favicon/android-icon-192x192.png')}}">
-<link rel="icon" type="image/png"  sizes="32x32"         href="href="{{asset('img/favicon/favicon-32x32.png')}}">
-<link rel="icon" type="image/png"  sizes="96x96"         href="href="{{asset('img/favicon/favicon-96x96.png')}}">
-<link rel="icon" type="image/png"  sizes="16x16"         href="href="{{asset('img/favicon/favicon-16x16.png')}}">
+<link rel="apple-touch-icon"       sizes="57x57"         href="{{asset('img/favicon/apple-icon-57x57.png')}}">
+<link rel="apple-touch-icon"       sizes="60x60"         href="{{asset('img/favicon/apple-icon-60x60.png')}}">
+<link rel="apple-touch-icon"       sizes="72x72"         href="{{asset('img/favicon/apple-icon-72x72.png')}}">
+<link rel="apple-touch-icon"       sizes="76x76"         href="{{asset('img/favicon/apple-icon-76x76.png')}}">
+<link rel="apple-touch-icon"       sizes="114x114"       href="{{asset('img/favicon/apple-icon-114x114.png')}}">
+<link rel="apple-touch-icon"       sizes="120x120"       href="{{asset('img/favicon/apple-icon-120x120.png')}}">
+<link rel="apple-touch-icon"       sizes="144x144"       href="{{asset('img/favicon/apple-icon-144x144.png')}}">
+<link rel="apple-touch-icon"       sizes="152x152"       href="{{asset('img/favicon/apple-icon-152x152.png')}}">
+<link rel="apple-touch-icon"       sizes="180x180"       href="{{asset('img/favicon/apple-icon-180x180.png')}}">
+<link rel="icon" type="image/png"  sizes="192x192"       href="{{asset('img/favicon/android-icon-192x192.png')}}">
+<link rel="icon" type="image/png"  sizes="32x32"         href="{{asset('img/favicon/favicon-32x32.png')}}">
+<link rel="icon" type="image/png"  sizes="96x96"         href="{{asset('img/favicon/favicon-96x96.png')}}">
+<link rel="icon" type="image/png"  sizes="16x16"         href="{{asset('img/favicon/favicon-16x16.png')}}">
 <link rel="manifest"                href="href="{{asset('img/favicon//manifest.json")}}">
 <meta name="msapplication-TileColor"    content="#ffffff">
 <meta name="msapplication-TileImage"    content="href="{{asset('img/favicon//ms-icon-144x144.png")}}">
